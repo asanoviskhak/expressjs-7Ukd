9b6c04379874bd849cd39d0d0383da80992683f2
Iskhak Asanov <asanov.personal@gmail.com>
2023-05-24 00:24:40 +0600

    :tada: OpenAI completions endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 export const app = express();
 
@@ -18,6 +19,34 @@ const api = express.Router();
 
 api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
+});
+
+/// make request to the openAi completion api which will take params from the request body = {prompt: string, max_tokens: number} using axios
+api.post('/openai', async (req, res) => {
+  console.log(req.body);
+  const { prompt, max_tokens, model } = req.body;
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/completions',
+      {
+        prompt,
+        max_tokens,
+        temperature: 0.9,
+        model: model || 'text-davinci-003'
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+        }
+      }
+    );
+    console.log(response.data);
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
 });
 
 // Version the api
